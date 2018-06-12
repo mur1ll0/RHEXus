d28b51e1141cb4ae5041a67126a4cddbe0668c70
roacosta <digo_acosta@hotmail.com>
2018-06-12 20:34:13 -0300

    Implementada chamada para o cadastro de pessoas

---
+++ app/src/main/java/rhexa/rhexus/PessoaListActivity.java
@@ -0,0 +1,13 @@
+package rhexa.rhexus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PessoaListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pessoa_list);
+    }
+}
