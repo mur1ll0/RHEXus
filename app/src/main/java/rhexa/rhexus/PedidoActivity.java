82a454a7907045bb9ca09201c8389d02c253484f
JeanFenner <jeancarlo.fenner@gmail.com>
2018-06-18 21:55:31 -0300

    ..

--- app/src/main/java/rhexa/rhexus/PedidoActivity.java
+++ app/src/main/java/rhexa/rhexus/PedidoActivity.java
@@ -1,8 +1,11 @@
 package rhexa.rhexus;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,6 +16,20 @@ public class PedidoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pedido_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        final NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ///MODO GAMBIARRA AppBar FUNCIONANDO
         ImageView nav_menu = findViewById(R.id.nav_menu);
@@ -23,7 +40,7 @@ public class PedidoActivity extends AppCompatActivity {
             }
 
         });
-        
+
         final TextView produtos = (TextView) findViewById(R.id.produtos);
         final TextView finalizar = (TextView) findViewById(R.id.finalizar);
 
