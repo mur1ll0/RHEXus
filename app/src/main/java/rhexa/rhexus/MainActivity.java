d1b1ab1285c9a83ae6034ae0acf4de4dc01d9613
mur1ll0 <murillomaleski@gmail.com>
2018-06-24 21:01:53 -0300

    Drawer para NewProd
    Drawer funcionando para qualquer layout. Basta criar um Drawer para cada tela.

--- app/src/main/java/rhexa/rhexus/MainActivity.java
+++ app/src/main/java/rhexa/rhexus/MainActivity.java
@@ -33,17 +33,8 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-
 
         final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
 
         final NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -112,39 +103,6 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- app/src/main/java/rhexa/rhexus/newProdActivity.java
+++ app/src/main/java/rhexa/rhexus/newProdActivity.java
@@ -1,27 +1,63 @@
 package rhexa.rhexus;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
-public class newProdActivity extends AppCompatActivity {
+public class newProdActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_prod_layout);
+        setContentView(R.layout.drawer_newprod);
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        final NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ///MODO GAMBIARRA AppBar FUNCIONANDO
         ImageView nav_menu = findViewById(R.id.nav_menu);
         nav_menu.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
-                //drawer.openDrawer(navigationView);
+                drawer.openDrawer(navigationView);
             }
 
         });
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        Intent it = null;
+
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.nav_itens) {
+            it = getIntent();
+            finish();
+        } else if (id == R.id.nav_people) {
+            it = new Intent(this, PessoaListActivity.class);
+        } else if (id == R.id.nav_pedido) {
+            it = new Intent(this, PedidoActivity.class);
+        } else if (id == R.id.nav_opt) {
+
+        } else if (id == R.id.nav_share) {
+
+        }
+
+        startActivity(it);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
